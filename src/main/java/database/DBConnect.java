147b4b2da66ff3a9dcb9e534f271f52c7ddcf2a7
Zhoraev D <newlifenewgoal@gmail.com>
2019-12-19 17:01:57 +0600

    jdbc

--- src/main/java/database/DBConnect.java
+++ src/main/java/database/DBConnect.java
@@ -5,26 +5,49 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DBConnect {
 
-    private final String url = "jdbc:mysql://localhost:3306";
+    private final String url = "jdbc:mysql://localhost:3306/information_schema";
     private final String user = "root";
-    private final String password = "root";
+    private final String password = "birone89";
     
     private static Connection conn;
     private static Statement stmt;
     private static ResultSet rs;
     
+    private List databases;
+    
     public DBConnect(){
+        
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+        databases = new ArrayList();
+        
         try {
             conn =  DriverManager.getConnection(url, user, password);
             stmt = conn.createStatement();
+            rs = stmt.executeQuery("SHOW DATABASES");
+            System.out.println(rs.toString());
+            while(rs.next()){
+                System.out.println(rs.next() +  " console: " );
+                databases.add(rs.next());
+            }
             
         } catch (SQLException ex) {
-            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
+    }
+    
+    public List getDatabases(){
+        return databases;
     }
 }
--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -1,18 +1,33 @@
 package servlet;
 
+import database.DBConnect;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       // resp.getWriter().write("Some");
+      resp.getWriter().write("Some");
 //        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/public/jsp/index.jsp");
 //        requestDispatcher.forward(req, resp);
-        resp.getWriter().write("Yandex vs Googel");
+           DBConnect db = new DBConnect();
+           System.out.println("Hello");
+           List list = db.getDatabases();
+           
+                resp.getWriter().write(list.size() + " dd");
+           
+//           for(Object database : list){
+//               System.out.println(database.toString());
+//               resp.getWriter().write(database.toString());
+//           }
+           
+           
     }
 }
