3148e8235d5ae5433d528789a6276d38786b6022
Zhoraev D <newlifenewgoal@gmail.com>
2020-02-20 17:10:35 +0600

    added logout page

---
+++ src/main/java/servlet/Logout.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import core.BaseHttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "Logout", urlPatterns = {"/logout"})
+
+
+public class Logout extends BaseHttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/jadminsql/auth");
+    }
+}
+
